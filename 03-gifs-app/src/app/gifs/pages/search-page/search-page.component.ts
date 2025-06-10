a8689ebb1ea695eedf11cb0fee2e72a945b4d7ef
nadinestrella <nadinestrella@gmail.com>
2025-06-10 11:48:05 +0200

    added img to trendinPg

---
+++ 03-gifs-app/src/app/gifs/pages/search-page/search-page.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-app',
+  imports: [],
+  templateUrl: './search-page.component.html',
+})
+export default class SearchAppComponent {}
