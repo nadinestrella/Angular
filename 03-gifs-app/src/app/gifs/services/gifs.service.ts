f8311c68a76ed24c7bffddfa09cbed621097f7b4
nadinestrella <nadinestrella@gmail.com>
2025-06-11 15:06:57 +0200

    gifs- added localStorage

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,30 @@
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { map, Observable, tap } from 'rxjs';
+
 import { environment } from '@environments/environment.development';
-import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
+import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { GifMapper } from '../mapper/gif.mapper';
+
+const GIF_KEY = 'gifs';
+
+const loadFromLocalStorage = () => {
+  // const gifsFromLocalStorage = localStorage.getItem(GIF_KEY) ?? '{}'; //Record<string, gifs[]>
+  // const gifs = JSON.parse(gifsFromLocalStorage);
+  // console.log(gifs);
+  // return gifs;
+  try {
+    const gifsFromLocalStorage = localStorage.getItem(GIF_KEY);
+    if (!gifsFromLocalStorage || gifsFromLocalStorage === 'undefined') {
+      return {};
+    }
+    return JSON.parse(gifsFromLocalStorage);
+  } catch (error) {
+    console.error('Error parsing localStorage data:', error);
+    return {};
+  }
+};
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
@@ -13,10 +34,22 @@ export class GifService {
   trendingGifs = signal<Gif[]>([]);
   trendingGifsLoading = signal(true);
 
+  searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
+
+  //senal computada
+  searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
+
   constructor() {
     //sirve para inicializar el servicio haciendo una primera carga de gifs
     this.loadTrendingGifs();
   }
+
+  saveGifsToLocalStorage = effect(() => {
+    const historyString = JSON.stringify(this.searchHistory());
+    localStorage.setItem(GIF_KEY, historyString);
+  });
+
+  // LoadGifs
 
   loadTrendingGifs() {
     // Peticion post,put,patch,get
@@ -37,8 +70,10 @@ export class GifService {
       });
   }
 
-  searchGifs(query: string) {
-    this.http
+  // Search
+
+  searchGifs(query: string): Observable<Gif[]> {
+    return this.http
       .get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
         params: {
           api_key: environment.giphyApiKey,
@@ -46,11 +81,23 @@ export class GifService {
           q: query,
         },
       })
-      //para que se dispare la peticion http necesitas subcribirte
-      .subscribe((resp) => {
-        const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
 
-        // console.log({ search: gifs });
-      });
+      .pipe(
+        map(({ data }) => data),
+        map((items) => GifMapper.mapGiphyItemsToGifArray(items)),
+        //efecto secundario para el historial
+        tap((items) => {
+          this.searchHistory.update((history) => ({
+            ...history,
+            [query.toLowerCase()]: items,
+          }));
+        })
+      );
+  }
+
+  // History
+
+  getHistoryGifs(query: string) {
+    return this.searchHistory()[query] ?? [];
   }
 }
